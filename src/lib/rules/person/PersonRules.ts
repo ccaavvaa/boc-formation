8b66e43e02eaabc48e6fc4ae033dce68a0e0e282
Catalin VASCU <ccaavvaa@gmail.com>
2018-12-21 10:38:55 +0100

    #

--- src/lib/rules/person/PersonRules.ts
+++ src/lib/rules/person/PersonRules.ts
@@ -86,4 +86,11 @@ export class PersonRules {
         }
         return this.isManager(manager, m);
     }
+    @boc.Action({
+        constr: Person,
+        actionId: 'remove',
+    })
+    public static async remove(target: Person, msg: boc.Message) {
+        await target.toDelete();
+    }
 }
